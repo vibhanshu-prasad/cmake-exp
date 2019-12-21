0628b39a3a1a4620204cf46481ef335ab3149cb8
deepcoder007 <vibhanshuprasad@gmail.com>
2019-12-21 12:08:54 +0530

    init commit

---
+++ include/header.h
@@ -0,0 +1,5 @@
+#include<iostream>
+
+int f() {
+    std::cout << " FUNCTION F() called " << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "header.h"
+
+int main() {
+    f();
+    std::cout << "Hello World" << std::endl
+        << __VERSION__ << std::endl;
+    return 0;
+}
